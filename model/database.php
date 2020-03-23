1aad236ec2f4d3680641d2df782f934881821a92
Wiseclone <40637778+Wiseclone@users.noreply.github.com>
2020-03-22 23:41:30 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
-    $password = '';
+    $dsn = 'mysql:host=k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=h6nkps6dwhzechkw';
+    $username = 'do74nxeiek1wev3i';
+    $password = 'e5e9vrj16eskrc9j';
 
     try {
         $db = new PDO($dsn, $username, $password);
